952904478b2cf311aff50cf1c40fc9c4570b8e8b
success353 <gmacro372@gmail.com>
2022-10-02 00:02:29 +0100

    modified:   components/header.js

--- components/header.js
+++ components/header.js
@@ -3,6 +3,7 @@ import Image from 'next/image'
 const Header = () => {
     return (
         <header className="">
+            <div></div>
             <h1>This is the Header</h1>
             <Image src='https://links.papareact.com/ua6' alt='' width={200} height={100} className='object-contain' />
         </header>
