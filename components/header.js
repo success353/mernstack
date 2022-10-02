cd846341b2079252b7a68cfac10412ef0f251f1b
success353 <gmacro372@gmail.com>
2022-10-02 01:03:13 +0100

    modified:   components/header.js 	modified:   components/header.js 	modified:   package-lock.json 	modified:   package.json 	modified:   yarn.lock 	components/HeaderItem.js

--- components/header.js
+++ components/header.js
@@ -3,7 +3,9 @@ import Image from 'next/image'
 const Header = () => {
     return (
         <header className="">
-            <div></div>
+            <div>
+                <HeaderItem />
+            </div>
             <h1>This is the Header</h1>
             <Image src='https://links.papareact.com/ua6' alt='' width={200} height={100} className='object-contain' />
         </header>
